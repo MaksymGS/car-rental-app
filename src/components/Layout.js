ee7252c0be4b36b2372dfa059ecbd560d57a8a9f
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-02-26 00:20:20 +0200

    add routes favorites

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,7 @@ export const Layout = () => {
         <nav>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/catalog">Catalog</NavLink>
+          <NavLink to="/favorites">Favorites</NavLink>
         </nav>
       </header>
       <Suspense fallback={null}>
